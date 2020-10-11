a5d2eb43d81ed47cfd8b3fb139d9ab52d82d7daf
NamekianPOSTBOY <rodrigues_brandon@hotmail.com>
2020-10-11 03:39:44 -0400

    Welcome screen, french support, beginning homes menus screen

---
+++ Assignment2/src/main/java/brandon/rodrigues/s301126222/brandonActivity.java
@@ -0,0 +1,32 @@
+package brandon.rodrigues.s301126222;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+import android.os.Bundle;
+
+public class brandonActivity extends AppCompatActivity {
+private Button enterBttn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterBttn = (Button)  findViewById(R.id.Brandon_MMbutton);
+        enterBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rodriguesActivityHomes();
+            }
+        });
+    }
+
+    public void rodriguesActivityHomes(){
+
+        Intent intentHomes = new Intent(this, rodriguesActivityHomes.class);
+        startActivity(intentHomes);
+    }
+}
